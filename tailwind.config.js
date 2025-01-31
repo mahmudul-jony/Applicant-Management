7224d55e6ef2e857f978925b501670441543c424
mahmudul-jony <mahmudul.jony1@gmail.com>
2025-02-01 02:12:11 +0600

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+
+    colors: {
+      'white': '#FFFFFF',
+      'neutral-50': '#FAFAFA',
+      'neutral-100': '#F5F5F5',
+      'neutral-200': '#E5E5E5',
+      'neutral-400': '#A3A3A3',
+      'neutral-600': '#525252',
+      'neutral-900': '#171717',
+      'teal-50': '#f0fdfa',
+      'teal-100': '#ccfbf1',
+      'teal-200': '#99F6E4',
+      'teal-700': '#0F766E',
+      'teal-800': '#115e59',
+      'yellow-500': '#EAB308',
+      'red-50': '#FEF2F2',
+      'red-100': '#FEE2E2',
+      'red-300': '#D6D3D1',
+      'red-600': '#DC2626',
+    },
+
+    fontFamily: {
+      'serif': ['Roboto', 'Georgia',],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
