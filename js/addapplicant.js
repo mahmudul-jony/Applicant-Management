f536140d52c54601d66176c7b7ff9df886255547
mahmudul-jony <mahmudul.jony1@gmail.com>
2025-02-07 18:08:02 +0600

    add applicant modal

---
+++ js/addapplicant.js
@@ -0,0 +1,50 @@
+// script for middle section
+function openNav() {
+document.getElementById("add-Applicant").style.display = "block";
+document.getElementById("applicant-list").style.display = "none";
+}
+
+function closeNav() {
+document.getElementById("add-Applicant").style.display = "none";
+document.getElementById("applicant-list").style.display = "block";
+}
+
+
+// script for tab
+function openmain(evt, main) {
+var i, tabcontent, tablinks;
+
+tabcontent = document.getElementsByClassName("tabcontent");
+for (i = 0; i < tabcontent.length; i++) {
+tabcontent[i].style.display = "none";
+}
+
+tablinks = document.getElementsByClassName("tablinks");
+for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].classList.remove("text-teal-800", "border-b", "border-teal-800");
+}
+
+document.getElementById(main).style.display = "block";
+
+evt.currentTarget.classList.add("text-teal-800", "border-b", "border-teal-800");
+}
+
+document.getElementById("Activity-tab").click();
+
+
+// script for modal
+// let modal = document.getElementById("add-applicant-modal");
+//     let btn = document.getElementById("add-Applicants");
+//     let button = document.getElementById("Close");
+
+//     btn.onclick = function () {
+//       modal.style.display = "block";
+//     };
+//     button.onclick = function () {
+//       modal.style.display = "none";
+//     };
+//     window.onclick = function (event) {
+//       if (event.target == modal) {
+//         modal.style.display = "none";
+//       }
+//     };
