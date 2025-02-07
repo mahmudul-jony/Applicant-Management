3905b713b90c4480a5b60488a0bb3915ea59b9e9
mahmudul-jony <mahmudul.jony1@gmail.com>
2025-02-07 20:24:18 +0600

    UI Update

--- js/addapplicant.js
+++ js/addapplicant.js
@@ -10,24 +10,24 @@ document.getElementById("applicant-list").style.display = "block";
 }
 
 
-// script for tab
-function openmain(evt, main) {
-var i, tabcontent, tablinks;
+// // script for tab
+// function openmain(evt, main) {
+// var i, tabcontent, tablinks;
 
-tabcontent = document.getElementsByClassName("tabcontent");
-for (i = 0; i < tabcontent.length; i++) {
-tabcontent[i].style.display = "none";
-}
+// tabcontent = document.getElementsByClassName("tabcontent");
+// for (i = 0; i < tabcontent.length; i++) {
+// tabcontent[i].style.display = "none";
+// }
 
-tablinks = document.getElementsByClassName("tablinks");
-for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].classList.remove("text-teal-800", "border-b", "border-teal-800");
-}
+// tablinks = document.getElementsByClassName("tablinks");
+// for (i = 0; i < tablinks.length; i++) {
+//     tablinks[i].classList.remove("text-teal-800", "border-b", "border-teal-800");
+// }
 
-document.getElementById(main).style.display = "block";
+// document.getElementById(main).style.display = "block";
 
-evt.currentTarget.classList.add("text-teal-800", "border-b", "border-teal-800");
-}
+// evt.currentTarget.classList.add("text-teal-800", "border-b", "border-teal-800");
+// }
 
 document.getElementById("Activity-tab").click();
 
