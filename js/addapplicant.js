ab3ed92c1ae3d2bc07c8b9956c215d6828ed28bc
mahmudul-jony <mahmudul.jony1@gmail.com>
2025-02-09 03:40:00 +0600

    UI Update

--- js/addapplicant.js
+++ js/addapplicant.js
@@ -9,26 +9,47 @@ document.getElementById("add-Applicant").style.display = "none";
 document.getElementById("applicant-list").style.display = "block";
 }
 
-// script for menu
-function toggleMenu() {
-    var menu = document.getElementById("popoverMenu");
-    if (menu.style.display === "flex") {
-        menu.style.display = "none";
-    } else {
-        menu.style.display = "flex";
-    }
+// script for onboard modal
+function openonboard() {
+document.getElementById("applicant-onboard").style.display = "block";
 }
 
-// Close the menu when clicking outside
-document.addEventListener("click", function(event) {
-    var menu = document.getElementById("popoverMenu");
-    var button = document.querySelector(".menu-button");
-    
-    if (!button.contains(event.target) && !menu.contains(event.target)) {
-        menu.style.display = "none";
-    }
-});
+function closeonboard() {
+document.getElementById("applicant-onboard").style.display = "none";
+}
 
+// // script for menu
+const button = document.getElementById("btn-more");
+        const popover = document.getElementById("popoverMenu");
+
+        function togglePopover() {
+            if (popover.style.display === "none" || popover.style.display === "") {
+                const rect = button.getBoundingClientRect();
+                popover.style.top = `${rect.bottom + window.scrollY + 8}px`;
+                popover.style.left = `${rect.left + window.scrollX + -136}px`;
+                popover.style.display = "block";
+            } else {
+                popover.style.display = "none";
+            }
+        }
+
+        // Show/Hide Popover on button click
+        button.addEventListener("click", (event) => {
+            event.stopPropagation(); // Prevent clicking button from triggering document click
+            togglePopover();
+        });
+
+        // Hide popover when clicking a menu option
+        popover.addEventListener("click", () => {
+            popover.style.display = "none";
+        });
+
+        // Hide popover when clicking outside
+        document.addEventListener("click", (event) => {
+            if (!popover.contains(event.target) && event.target !== button) {
+                popover.style.display = "none";
+            }
+        });
 
 
 // script for tab
