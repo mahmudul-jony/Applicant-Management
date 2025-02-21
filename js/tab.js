16a0afc1ca9cebfcbded1dc335a55a0debb90857
mahmudul-jony <mahmudul.jony1@gmail.com>
2025-02-22 00:25:26 +0600

    update ui

--- js/tab.js
+++ js/tab.js
@@ -6,17 +6,15 @@ function openTab(event, tabId) {
     
     // Remove active styling from all buttons
     document.querySelectorAll('.tab-button').forEach(button => {
-        button.classList.remove('bg-blue-600', 'text-white');
-        button.classList.add('bg-gray-400', 'text-black');
+        button.classList.remove('active');
     });
     
     // Show the selected tab and apply active styling to the button
     document.getElementById(tabId).classList.remove('hidden');
-    event.currentTarget.classList.add('bg-blue-600', 'text-white');
-    event.currentTarget.classList.remove('bg-gray-400', 'text-black');
+    event.currentTarget.classList.add('active');
 }
 
 // Set the default active tab on page load
 document.addEventListener("DOMContentLoaded", function() {
-    document.querySelector(".tab-button").click();
+    document.querySelector('.tab-button.btn-tab.active').click();
 });
